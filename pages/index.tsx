508c27077e8fdbe89b3014a11cb8dedfe3ed250e
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-09-27 17:07:00 +0500

    add button style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import { Test } from '@components/Test'
-import { Button } from '@mantine/core'
+import { Button, Container, Stack } from '@mantine/core'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<Button>Mantine button</Button>
-			<Test />
-		</div>
+		<Container pt="xl">
+			<Stack align="flex-start">
+				<Button>Mantine button</Button>
+				<Button variant="filled">filled button</Button>
+				<Button variant="light">light button</Button>
+				<Button variant="outline">outline button</Button>
+				<Button variant="subtle">subtle button</Button>
+				<Button variant="white">white button</Button>
+				<Button disabled>disabled button</Button>
+				<Button loading>loading button</Button>
+			</Stack>
+		</Container>
 	)
 }
 
--- styles/mantineTheme.ts
+++ styles/mantineTheme.ts
@@ -1,14 +1,45 @@
-import { ButtonProps, MantineThemeOverride } from '@mantine/core'
+import {
+	ButtonProps,
+	ButtonStylesParams,
+	MantineThemeOverride,
+} from '@mantine/core'
 
-const ButtonDefaultProps: Partial<ButtonProps> = {
-	radius: 'xs',
-}
+const ButtonDefaultProps: Partial<ButtonProps> = {}
 
 export const mantineTheme: MantineThemeOverride = {
-	colors: {},
+	colors: {
+		brand: [
+			'#F2F5F9',
+			'#E5EBF3',
+			'#C9D5E7',
+			'#A7BEDA',
+			'#7DA2CD',
+			'#3B82BF',
+			'#3474AA',
+			'#2D6493',
+			'#255278',
+			'#1A3A55',
+		],
+	},
+	primaryColor: 'brand',
+	primaryShade: 5,
 	components: {
 		Button: {
 			defaultProps: ButtonDefaultProps,
+			styles: (theme, params: ButtonStylesParams) => ({
+				root: {
+					borderRadius: '4px',
+					boxShadow:
+						params.variant === 'filled'
+							? `inset 0px -2px 0px ${theme.colors.brand[7]}`
+							: undefined,
+					'&:disabled': {
+						backgroundColor: theme.colors.brand[3],
+						color: 'white',
+						boxShadow: `inset 0px -2px 0px ${theme.colors.brand[4]}`,
+					},
+				},
+			}),
 		},
 	},
 }
