aa925fc09388073aa7bc80c9416b8bc3b6d897e3
Vlad Gafarov <vlad.gafarov02@gmail.com>
2022-10-08 21:21:15 +0500

    delete test file

--- __tests__/sample.test.tsx
+++ __tests__/sample.test.tsx
@@ -1,14 +1,7 @@
-import { Test } from '@components/Test'
-import { render } from '@testing-library/react'
-
 describe('sample test', () => {
 	it('should pass', () => {
 		expect(true).toBe(true)
 	})
-
-	it('renders correctly', () => {
-		const { container } = render(<Test />)
-
-		expect(container).toBeInTheDocument()
-	})
 })
+
+export {}
